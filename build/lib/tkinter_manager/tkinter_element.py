352c3b98e1879ddbb24e1332e75788fc160980bf
ScriptedChicken <angusfhunt@gmail.com>
2024-03-10 08:37:20 +1300

    Release of v3.0.1

--- build/lib/tkinter_manager/tkinter_element.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Union, Callable
-
-class TKinterElement(object):
-    def __init__(self, element_name: str, element_object: Any, hook: Union[Callable, None]=None):
-        self.name = element_name
-        self.element_object = element_object
-        self.hook = hook
--- build/lib/tkinter_manager/utils.py
+++
@@ -1,6 +0,0 @@
-def return_clean_text(input_text: str) -> str:
-    return input_text.replace("_", " ").capitalize()
-
-def return_bind_event(input_text: str) -> str:
-    
-    return shortcut.notation
